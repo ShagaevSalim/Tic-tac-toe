fd45a04c0d469ecf65f80da95038ea91c5cd9078
Salim Shagaev <salim.shagaev.06@gmail.com>
2024-10-07 19:47:55 +0300

    Реализована логика MainActivity Добавлены обработчики для кнопок

--- app/src/main/java/com/shag/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/shag/tic_tac_toe/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.shag.tic_tac_toe
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.shag.tic_tac_toe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.contsraintLayout)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.toNewGame.setOnClickListener{
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+        binding.toContinueGame.setOnClickListener{
+            val data = getInfoAboutGame()
+            val intent = Intent(this, GameActivity::class.java).apply{
+                putExtra(EXTRA_TIME, data.time)
+                putExtra(EXTRA_GAME_FIELD, data.gameField)
+            }
+            startActivity(intent)
+        }
+        binding.toSettings.setOnClickListener{
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        setContentView(binding.root)
+    }
+
+    private fun getInfoAboutGame(): InfoGame{
+        with(getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)){
+            val time = getLong("time", 0L)
+            val gameField = getString("gameField", "")
+
+            return if(gameField!=null){
+                InfoGame(time, gameField)
+            }else{
+                InfoGame(0, "")
+            }
         }
     }
+    data class InfoGame(
+        val time: Long,
+        val gameField: String,
+    )
+
+    companion object{
+        const val EXTRA_TIME = "extra_time"
+        const val EXTRA_GAME_FIELD = "extra_game_field"
+    }
+
 }
