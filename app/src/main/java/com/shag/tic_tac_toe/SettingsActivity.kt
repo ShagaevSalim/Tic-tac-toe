9f3e58db6b50b008b4ea340d18cf523acd09aa59
Salim Shagaev <salim.shagaev.06@gmail.com>
2024-10-07 20:19:40 +0300

    Добавлено сокрытие и отображение UI-элементов (кнопок) внутри метода OnCreate() , а также отображение информации о выбранном уровнем сложности
    Добавлено восстановление настроек

--- app/src/main/java/com/shag/tic_tac_toe/SettingsActivity.kt
+++ app/src/main/java/com/shag/tic_tac_toe/SettingsActivity.kt
@@ -1,20 +1,182 @@
 package com.shag.tic_tac_toe
 
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.shag.tic_tac_toe.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    private var currentSoundLevel = 0
+    private var currentLvl = 0
+    private var currentRules = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.contsraintLayout)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+
+        val data = getSettingsInfo()
+
+        currentSoundLevel = data.soundLvl
+        currentRules = data.rules
+        currentLvl = data.lvl
+
+        when(currentRules){
+            1 -> binding.checkboxVertical.isChecked = true
+            2 -> binding.checkboxHorizontal.isChecked = true
+            3 -> {
+                binding.checkboxVertical.isChecked = true
+                binding.checkboxHorizontal.isChecked = true
+            }
+            4 -> binding.checkboxDiagonal.isChecked = true
+            5 -> {
+                binding.checkboxDiagonal.isChecked = true
+                binding.checkboxVertical.isChecked = true
+            }
+            6 -> {
+                binding.checkboxDiagonal.isChecked = true
+                binding.checkboxHorizontal.isChecked = true
+            }
+            7 -> {
+                binding.checkboxVertical.isChecked = true
+                binding.checkboxHorizontal.isChecked = true
+                binding.checkboxDiagonal.isChecked = true
+            }
+        }
+
+        if(currentLvl == 0){
+            binding.prevLvl.visibility = View.INVISIBLE
+        }else if(currentLvl == 2){
+            binding.nextLvl.visibility = View.INVISIBLE
+        }
+
+        binding.infoLevel.text = resources.getStringArray(R.array.game_level)[currentLvl]
+        binding.soundBar.progress = currentSoundLevel
+
+
+        binding.prevLvl.setOnClickListener{
+            currentLvl--
+
+            if(currentLvl == 0){
+                binding.prevLvl.visibility = View.INVISIBLE
+            }else if(currentLvl == 1){
+                binding.nextLvl.visibility = View.VISIBLE
+            }
+
+            binding.infoLevel.text = resources.getStringArray(R.array.game_level)[currentLvl]
+
+
+            updateLvl(currentLvl)
+        }
+
+        binding.nextLvl.setOnClickListener{
+            currentLvl++
+
+            if(currentLvl==1){
+                binding.prevLvl.visibility = View.VISIBLE
+            }else if (currentLvl == 2){
+                binding.nextLvl.visibility = View.INVISIBLE
+            }
+
+            binding.infoLevel.text = resources.getStringArray(R.array.game_level)[currentLvl]
+
+            updateLvl(currentLvl)
+        }
+
+        binding.soundBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, value: Int, p2: Boolean) {
+                currentSoundLevel = value
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                updateSoundValue(currentSoundLevel)
+            }
+        } )
+
+        binding.checkboxVertical.setOnCheckedChangeListener{_, isChecked ->
+            if(isChecked){
+                currentRules++
+            }else{
+                currentRules--
+            }
+
+
+
+            updateRules(currentRules)
+        }
+
+        binding.checkboxHorizontal.setOnCheckedChangeListener{_, isChecked ->
+            if(isChecked){
+                currentRules+=2
+            }else{
+                currentRules-=2
+            }
+
+            updateRules(currentRules)
+
+        }
+
+        binding.checkboxDiagonal.setOnCheckedChangeListener{_, isChecked ->
+            if(isChecked){
+                currentRules+=4
+            }else{
+                currentRules-=4
+            }
+
+            updateRules(currentRules)
+
+        }
+
+
+        setContentView(binding.root)
+    }
+
+    private fun updateSoundValue(value: Int){
+        with(getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE).edit()){
+            putInt("sound_value", value)
+            apply()
         }
     }
+
+    private fun updateLvl(lvl: Int){
+        with(getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE).edit()){
+            putInt("lvl", lvl)
+            apply()
+        }
+    }
+
+    private fun updateRules(rules: Int){
+        with(getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE).edit()){
+            putInt("rules", rules)
+            apply()
+        }
+    }
+
+    private fun getSettingsInfo(): SettingsInfo
+    {
+        with(getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)){
+            val soundLvl = getInt("sound_lvl", 0)
+            val lvl = getInt("lvl", 0)
+            val rules = getInt("rules", 0)
+
+            return SettingsInfo(soundLvl, lvl, rules)
+        }
+    }
+
+    data class SettingsInfo(
+        val soundLvl: Int,
+        val lvl: Int,
+        val rules: Int,
+    )
 }
